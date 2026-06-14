07f96949e12e9e087dccbbcab06022c1a16c6313
yusuf601 <yusufmuhyusuh@gmail.com>
2026-06-15 02:15:05 +0700

    Refactor sidebar: Nest Bank Modul as submenu of Kelas Saya

--- app/Views/bank_modul.php
+++ app/Views/bank_modul.php
@@ -565,14 +565,16 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg>
                             </span>
                         </a>
-                    </li>
-                    <li class="sidebar-menu-item active">
-                        <a href="/rpl/public/index.php?action=bank_modul">
-                            <span>Bank Modul</span>
-                            <span class="sidebar-menu-item-icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
-                            </span>
-                        </a>
+                        <ul class="sidebar-submenu-list">
+                            <li class="sidebar-submenu-item active">
+                                <a href="/rpl/public/index.php?action=bank_modul">
+                                    <span>Bank Modul</span>
+                                    <span class="sidebar-menu-item-icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
                     </li>
                     <li class="sidebar-menu-item">
                         <a href="#">
