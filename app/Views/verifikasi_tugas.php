2ed13e65d3bcb1f0e0fa6c5c5107d1bdfb8c3c96
yusuf601 <yusufmuhyusuh@gmail.com>
2026-06-20 15:31:32 +0700

    refactor(views): align Asisten sidebar menus with Figma designs

--- app/Views/verifikasi_tugas.php
+++ app/Views/verifikasi_tugas.php
@@ -522,6 +522,18 @@ $initials = substr($initials, 0, 2);
                                 </a>
                             </li>
                             <li class="sidebar-menu-item">
+                                <a href="/rpl/public/index.php?action=my_classes">
+                                    <span>Kelas Saya</span>
+                                    <span class="sidebar-menu-item-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 19.5A2.5 2.5 0 0 1 6.5 17H20"></path><path d="M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z"></path></svg></span>
+                                </a>
+                            </li>
+                            <li class="sidebar-menu-item">
+                                <a href="/rpl/public/index.php?action=data_kelompok">
+                                    <span>Data Kelompok</span>
+                                    <span class="sidebar-menu-item-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect><path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path></svg></span>
+                                </a>
+                            </li>
+                            <li class="sidebar-menu-item">
                                 <a href="/rpl/public/index.php?action=presensi">
                                     <span>Input Presensi</span>
                                     <span class="sidebar-menu-item-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg></span>
@@ -531,12 +543,6 @@ $initials = substr($initials, 0, 2);
                                 <a href="/rpl/public/index.php?action=verifikasi_tugas">
                                     <span>Verifikasi Tugas</span>
                                     <span class="sidebar-menu-item-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="9 11 12 14 22 4"></polyline><path d="M21 12v7a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11"></path></svg></span>
-                                </a>
-                            </li>
-                            <li class="sidebar-menu-item">
-                                <a href="/rpl/public/index.php?action=monitoring_kelas">
-                                    <span>Monitoring Kelas</span>
-                                    <span class="sidebar-menu-item-icon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect><line x1="16" y1="2" x2="16" y2="6"></line><line x1="8" y1="2" x2="8" y2="6"></line><line x1="3" y1="10" x2="21" y2="10"></line></svg></span>
                                 </a>
                             </li>
                             <li class="sidebar-menu-item">
